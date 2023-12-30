fa5407418612746cc6f9a89275cd71934c194f3f
jian0211 <myunggeun222@gmail.com>
2023-12-30 22:40:23 +0900

    setup-install:StyleX インストール及び設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+const stylexPlugin = require('@stylexjs/nextjs-plugin');
+
+module.exports = stylexPlugin({
+  rootDir: __dirname,
+})({});
