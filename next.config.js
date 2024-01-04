37613de04f5d653791742da844a5e8692a828edd
jian0211 <myunggeun222@gmail.com>
2024-01-05 00:30:24 +0900

    StyleX 設定

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
-const stylexPlugin = require('@stylexjs/nextjs-plugin');
 const withNextIntl = require('next-intl/plugin')();
+const stylexPlugin = require('@stylexjs/nextjs-plugin');
+
+const babelrc = require('./babelrc.js');
+const plugins = babelrc.plugins;
+const [_name, options] = plugins.find(
+  (plugin) => Array.isArray(plugin) && plugin[0] === '@stylexjs/babel-plugin',
+);
+const rootDir = options.unstable_moduleResolution.rootDir ?? __dirname;
 
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {};
+const nextConfig = {
+  // transpilePackages: ['@stylexjs/open-props'],
+  // pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+};
 
 module.exports = withNextIntl(
   stylexPlugin({
-    rootDir: __dirname,
+    rootDir,
   })(nextConfig),
 );
