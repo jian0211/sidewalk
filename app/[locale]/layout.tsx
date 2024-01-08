b74d262f6fa5c0e6c35d91776e44d71de3b3e722
jian0211 <myunggeun222@gmail.com>
2024-01-08 10:09:13 +0900

    初期Layout形作成

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,22 +1,8 @@
 import { Metadata } from 'next';
 import AppProvider from './provider';
 import '../../styles/global.css';
-import inject from '@stylexjs/dev-runtime';
 import * as stylex from '@stylexjs/stylex';
-
-/**
- * StyleX ローカルだけですること。
- * https://stylexjs.com/docs/learn/installation/#local-development-only
- * 現在StyleXについて設定がよくできなく、一旦これを使って後で解決をする。
- * いつか、Injectは削除予定
- */
-inject({
-  classNamePrefix: 'x',
-  dev: true,
-  test: false,
-  styleResolution: 'application-order',
-  useRemForFontSize: false,
-});
+import Sidebar from '@/components/templates/Sidebar';
 
 export const metadata: Metadata = {
   title: 'Sidewalk with flying',
@@ -35,19 +21,22 @@ export default function LocaleLayout({
 }: LocaleProps) {
   return (
     <html lang={locale}>
-      <body>
-        <nav>here is nav bar</nav>
-        <div {...stylex.props(mainStyles.test)}>ddd</div>
-
-        <AppProvider>{children}</AppProvider>
+      <body {...stylex.props(styles.body)}>
+        <Sidebar />
+        <div>
+          <nav>here is nav bar</nav>
+          <AppProvider>{children}</AppProvider>
+        </div>
       </body>
     </html>
   );
 }
 
-const mainStyles = stylex.create({
-  test: {
-    width: '100%',
+const styles = stylex.create({
+  body: {
+    width: '100vh',
+    height: '100vh',
+    display: 'flex',
     backgroundColor: 'pink',
   },
 });
