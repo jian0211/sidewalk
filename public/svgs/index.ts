cb15e5da6ba561c966304fc5d46a54bba0bb3874
jian0211 <myunggeun222@gmail.com>
2024-02-05 23:18:57 +0900

    nav:[search] Icon 作成

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -3,5 +3,12 @@ import IconJapanFlag from './Flag_of_Japan.svg';
 import IconKoreaFlag from './Flag_of_South_Korea.svg';
 import IconSwap from './booking__swap--disabled.svg';
 import IconFind from './find.svg';
+import IconDepartureFlight from './departures-flight.svg';
 
-export { IconJapanFlag, IconKoreaFlag, IconSwap, IconFind };
+export {
+  IconJapanFlag,
+  IconKoreaFlag,
+  IconSwap,
+  IconFind,
+  IconDepartureFlight,
+};
