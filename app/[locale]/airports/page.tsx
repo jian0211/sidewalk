64ed95006d263046b20832e704325a547f0c8958
jian0211 <myunggeun222@gmail.com>
2024-02-29 20:47:08 +0900

    project:掃除1

--- app/[locale]/airports/page.tsx
+++
@@ -1,9 +0,0 @@
-import Airport from '@/components/templates/airport/Airport';
-
-type AirportPageProps = React.ComponentPropsWithoutRef<'section'>;
-
-const AirportPage = (props: AirportPageProps) => {
-  return <Airport {...props} />;
-};
-
-export default AirportPage;
