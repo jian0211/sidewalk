ced8894c41bee7cc833346f9fc87faf1a86099f0
jian0211 <myunggeun222@gmail.com>
2024-03-09 01:19:11 +0900

    airport APIを用いて画面描く

--- app/[locale]/(airports)/airports/page.tsx
+++ app/[locale]/(airports)/airports/page.tsx
@@ -2,101 +2,112 @@ import {
   AirportsContainer,
   TitleWithAirportsInfo,
 } from '@/components/templates/airport/Airport';
-import { Page } from '@/types/page';
 import { Metadata } from 'next';
 import { Table } from '@/components/molecules/table/Table';
 import { useTranslatedWord } from '@/hooks/useTranslatedWord';
+import { Suspense } from 'react';
+import { Prisma, Role } from '@prisma/client';
 
 export const metadata: Metadata = {
   title: 'Airports | Sidewalk',
 };
 
-export const AirportPage = (props: Page) => {
+export const AirportsPage = async () => {
+  return (
+    <Suspense fallback={'loading'}>
+      <Airport />
+    </Suspense>
+  );
+};
+export default AirportsPage;
+
+const Airport = async () => {
   const t = useTranslatedWord('airports.info');
+  const airports = await getAirports();
+  const { japanAirports, koreaAirprots } = airports.reduce<{
+    japanAirports: Prisma.AirportCreateInput[];
+    koreaAirprots: Prisma.AirportCreateInput[];
+  }>(
+    (combinedAirports, airport) => {
+      const airportKey =
+        airport.countryCode === 'JP' ? 'japanAirports' : 'koreaAirprots';
+      combinedAirports[airportKey].push(airport);
+      return combinedAirports;
+    },
+    {
+      japanAirports: [],
+      koreaAirprots: [],
+    },
+  );
   return (
     <AirportsContainer>
       <TitleWithAirportsInfo>
         <h2>{t('japan')}</h2>
-        <span>{t('count')}:23</span>
+        <span>
+          {t('count')}:{japanAirports.length}
+        </span>
         <span>{t('updatedAt')}:2024/2/11</span>
       </TitleWithAirportsInfo>
-      <Table.Container>
+      {/* <Table.Container>
         <Table.Header>
-          <Table.Column flex="1">공항아이콘</Table.Column>
+          <Table.Column flex="1">공항이름</Table.Column>
           <Table.Column flex="2">공항이름</Table.Column>
           <Table.Column flex="1">IATA</Table.Column>
           <Table.Column flex="1">ICAO</Table.Column>
           <Table.Column flex="3">주소</Table.Column>
           <Table.Column flex="3">링크</Table.Column>
         </Table.Header>
-        <Table.Row>
-          <Table.Column flex="1">인천공항이미지</Table.Column>
-          <Table.Column flex="2">인천국제공항</Table.Column>
-          <Table.Column flex="1">ICN</Table.Column>
-          <Table.Column flex="1">RKSI</Table.Column>
-          <Table.Column flex="3">인천광역시 중구 제 1여객터미널</Table.Column>
-          <Table.Column flex="3">링크</Table.Column>
-        </Table.Row>
-        <Table.Row>
-          <Table.Column flex="1">인천공항이미지</Table.Column>
-          <Table.Column flex="2">인천국제공항</Table.Column>
-          <Table.Column flex="1">ICN</Table.Column>
-          <Table.Column flex="1">RKSI</Table.Column>
-          <Table.Column flex="3">인천광역시 중구 제 1여객터미널</Table.Column>
-          <Table.Column flex="3">링크</Table.Column>
-        </Table.Row>
-        <Table.Row>
-          <Table.Column flex="1">인천공항이미지</Table.Column>
-          <Table.Column flex="2">인천국제공항</Table.Column>
-          <Table.Column flex="1">ICN</Table.Column>
-          <Table.Column flex="1">RKSI</Table.Column>
-          <Table.Column flex="3">인천광역시 중구 제 1여객터미널</Table.Column>
-          <Table.Column flex="3">링크</Table.Column>
-        </Table.Row>
-      </Table.Container>
+        {japanAirports.map((airport, i) => (
+          <Table.Row key={i}>
+            <Table.Column flex="1">
+              <div>{airport.titleJa}</div>
+              <div>{airport.titleKo}</div>
+            </Table.Column>
+            <Table.Column flex="1">
+              {airport.iata + ' / ' + airport.icao}
+            </Table.Column>
+            <Table.Column flex="3">{airport.address}</Table.Column>
+            <Table.Column flex="3">인천광역시 중구 제 1여객터미널</Table.Column>
+            <Table.Column flex="3">{airport.link}</Table.Column>
+          </Table.Row>
+        ))}
+      </Table.Container> */}
       <TitleWithAirportsInfo>
         <h2>{t('korea')}</h2>
-        <span>{t('count')}:23</span>
+        <span>
+          {t('count')}:{koreaAirprots.length}
+        </span>
         <span>{t('updatedAt')}:2024/2/11</span>
       </TitleWithAirportsInfo>
       <Table.Container>
         <Table.Header>
-          <Table.Column flex="1">공항이미지</Table.Column>
           <Table.Column flex="2">공항이름</Table.Column>
-          <Table.Column flex="1">IATA</Table.Column>
-          <Table.Column flex="1">ICAO</Table.Column>
+          <Table.Column flex="2">IATA / ICAO</Table.Column>
+          <Table.Column flex="3">주소</Table.Column>
           <Table.Column flex="3">위치</Table.Column>
           <Table.Column flex="3">링크</Table.Column>
         </Table.Header>
-        <Table.Row>
-          <Table.Column flex="1">인천공항이미지</Table.Column>
-          <Table.Column flex="2">인천국제공항</Table.Column>
-          <Table.Column flex="1">ICN</Table.Column>
-          <Table.Column flex="1">RKSI</Table.Column>
-          <Table.Column flex="3">인천광역시 중구 제 1여객터미널</Table.Column>
-          <Table.Column flex="3">링크</Table.Column>
-        </Table.Row>
-        <Table.Row>
-          <Table.Column flex="1">인천공항이미지</Table.Column>
-          <Table.Column flex="2">인천국제공항</Table.Column>
-          <Table.Column flex="1">ICN</Table.Column>
-          <Table.Column flex="1">RKSI</Table.Column>
-          <Table.Column flex="3">인천광역시 중구 제 1여객터미널</Table.Column>
-          <Table.Column flex="3">링크</Table.Column>
-        </Table.Row>
-        <Table.Row>
-          <Table.Column flex="1">인천공항이미지</Table.Column>
-          <Table.Column flex="2">인천국제공항</Table.Column>
-          <Table.Column flex="1">ICN</Table.Column>
-          <Table.Column flex="1">RKSI</Table.Column>
-          <Table.Column flex="3">인천광역시 중구 제 1여객터미널</Table.Column>
-          <Table.Column flex="3">링크</Table.Column>
-        </Table.Row>
+        {koreaAirprots.map((airport, i) => (
+          <Table.Row key={i}>
+            <Table.Column flex="2">
+              <div>{airport.titleJa}</div>
+              <div>{airport.titleKo}</div>
+            </Table.Column>
+            <Table.Column flex="2">
+              {airport.iata + ' / ' + airport.icao}
+            </Table.Column>
+            <Table.Column flex="3">{airport.address}</Table.Column>
+            <Table.Column flex="3">{airport.longitude}</Table.Column>
+            <Table.Column flex="3">{airport.link}</Table.Column>
+          </Table.Row>
+        ))}
       </Table.Container>
     </AirportsContainer>
   );
 };
 
-export default AirportPage;
-
-const getAirports = async () => {};
+const getAirports = async (): Promise<Prisma.AirportCreateInput[]> => {
+  const url = `${process.env.DEV_API_BASE_URL}/airports`;
+  const airportsOfJapanAndKorea = await fetch(url);
+  return airportsOfJapanAndKorea.json();
+};
