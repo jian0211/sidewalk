d7c15299c5797fba1ea22c3f86c29f71fb11c7dc
jian0211 <myunggeun222@gmail.com>
2024-03-11 16:03:27 +0900

    要らない事削除

--- components/TestComp.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { mainTest } from '@/store/mainTest';
-import { useRecoilValue } from 'recoil';
-export const TestComp = () => {
-  const dd = useRecoilValue(mainTest);
-  return <div>{dd}deddddd</div>;
-};
