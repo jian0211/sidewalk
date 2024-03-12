dcab87d370b005b2f48f3aa1f4ee0263880fb93e
jian0211 <myunggeun222@gmail.com>
2024-03-12 21:12:56 +0900

    Change the layout component

--- app/[locale]/(airports)/airports/[country]/layout.tsx
+++ app/[locale]/(airports)/airports/[country]/layout.tsx
@@ -1,25 +1,5 @@
-import {
-  AirportsLayoutContainer,
-  AirportsLayoutLink,
-  AirportsLayoutTitle,
-} from '@/components/templates/airport/Airport';
-import { useTranslatedWord } from '@/hooks/useTranslatedWord';
 import { LayoutProps } from '../../../layout';
-import { path } from '@/types/path';
-import { Country } from '@/types/country';
 
 export default function Layout({ children }: LayoutProps) {
-  const t = useTranslatedWord('nav.pageTitle');
-  const getUrl = (country: Country) => `${path.airports}/${country}`;
-
-  return (
-    <>
-      <AirportsLayoutContainer>
-        <AirportsLayoutTitle>{t('airports')}</AirportsLayoutTitle>
-        <AirportsLayoutLink href={getUrl('jp')} title="일본" />
-        <AirportsLayoutLink href={getUrl('ko')} title="한국" />
-      </AirportsLayoutContainer>
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
--- components/templates/nav/Nav.tsx
+++ components/templates/nav/Nav.tsx
@@ -1,23 +1,51 @@
+'use client';
+
 import '@/hooks/useInjectStyleX';
 import * as stylex from '@stylexjs/stylex';
 import { SearchForm } from './search/SearchForm';
 import { LocaleSwitcher } from './localeConvert/LocaleSwitcher';
 import { EditIcon, NotificationIcon } from '@/components/atoms/Icon';
 import { ComponentPropsWithoutRef } from 'react';
+import { useCurrentPath } from '@/hooks/useCurrentPath';
+import Link from 'next/link';
+import { useTranslatedWord } from '@/hooks/useTranslatedWord';
+import { Country } from '@/types/country';
+import { path } from '@/types/path';
 
 type NavProps = React.ComponentPropsWithoutRef<'nav'>;
-type PageTitleProps = ComponentPropsWithoutRef<'h1'>;
+type AirportsLayoutContainerProps = ComponentPropsWithoutRef<'div'>;
 type NavGlobalEditBoxProps = React.ComponentPropsWithoutRef<'div'>;
 type NotificationBoxProps = React.ComponentPropsWithoutRef<'div'>;
 
 export const Nav = (props: NavProps) => {
+  const { isAirportPath, isAirlinePath, isFligths, isHomePath } =
+    useCurrentPath();
   return (
-    <nav {...stylex.props(styles.nav)}>
-      <SearchForm />
+    <nav {...stylex.props(styles.nav)} {...props}>
+      {isHomePath || isFligths ? (
+        <SearchForm />
+      ) : isAirportPath ? (
+        <AirportsLayoutContainer />
+      ) : isAirlinePath ? (
+        'airline'
+      ) : undefined}
       <LocaleSwitcher />
       <NavGlobalEditBox />
       <NotificationBox />
     </nav>
+  );
+};
+
+const AirportsLayoutContainer = (props: AirportsLayoutContainerProps) => {
+  const t = useTranslatedWord('nav.airports');
+  const getUrl = (country: Country) => `${path.airports}/${country}`;
+
+  return (
+    <div {...stylex.props(styles.airportsLayoutContainer)} {...props}>
+      <h2 {...stylex.props(styles.airportsLayoutTitle)}>{t('title')}</h2>
+      <Link href={getUrl('jp')}>{t('japan')}</Link>
+      <Link href={getUrl('ko')}>{t('korea')}</Link>
+    </div>
   );
 };
 
@@ -45,5 +73,13 @@ const styles = stylex.create({
     alignItems: 'center',
     padding: '0 3rem 0 2rem',
     gap: '1rem',
+  },
+  airportsLayoutContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    flex: '1',
+  },
+  airportsLayoutTitle: {
+    padding: '1.1rem',
   },
 });
