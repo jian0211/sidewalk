699942090591e332d2c437145e06d2a39eaed0ac
jian0211 <myunggeun222@gmail.com>
2024-03-18 21:35:12 +0900

    [token]: 関数生成

--- app/api/airlines/dummy/route.ts
+++ app/api/airlines/dummy/route.ts
@@ -7,13 +7,26 @@ type ResponseData = {
   airportsData: Prisma.AirportCreateInput[];
 };
 
+type ResponseTokenData = {
+  type: string; //'amadeusOAuth2Token';
+  username: string; //'foo@bar.com';
+  application_name: string; //'BetaTest_foobar';
+  client_id: string; //'3sY9VNvXIjyJYd5mmOtOzJLuL1BzJBBp';
+  token_type: 'Bearer';
+  access_token: string; //'CpjU0sEenniHCgPDrndzOSWFk5mN';
+  expires_in: string; // 1799;
+  state: string; //'approved';
+  scope: string; //'';
+};
+
 export async function GET(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>,
 ) {
   try {
-    const datat = await getAmadeusToken();
-    return Response.json(datat);
+    const token: ResponseTokenData['access_token'] = await getAmadeusToken();
+    const ssss = await getDummyAirlineData(token);
+    return Response.json(ssss);
   } catch (err) {
     console.log('err', err);
     // res.status(500).send({ error: 'Failed to fetch data' });
@@ -22,28 +35,49 @@ export async function GET(
 
 const getAmadeusToken = async () => {
   console.log('Start The getAmadeusToken ');
-  const tokenUrl = process.env.AMADEUS_API_TOKEN_URL ?? throwError('URL');
+  const tokenUrl = process.env.AMADEUS_API_TOKEN_URL ?? throwError('TOKEN_URL');
   const clientId = process.env.AMADEUS_API_KEY ?? throwError('KEY');
   const clientSecret = process.env.AMADEUS_API_SECRET ?? throwError('SECRET');
 
-  const token = await fetch(tokenUrl, {
+  const tokenResponse = await fetch(tokenUrl, {
     body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
     method: 'POST',
   });
-  console.log('token', token.json());
+
+  const tokenData: ResponseTokenData = await tokenResponse.json();
+  console.log('token', tokenData);
   console.log('End The getAmadeusToken ');
-  return token.json();
+  return tokenData.access_token;
 };
 
-const throwError = (type: 'URL' | 'KEY' | 'SECRET'): never => {
+const throwError = (type: 'TOKEN_URL' | 'KEY' | 'SECRET'): never => {
   const defaultMessage = 'AMADEUS env process Error.';
   const messages: Record<typeof type, string> = {
     KEY: `${defaultMessage} there is any API KEY`,
     SECRET: `${defaultMessage} there is any API SECRET`,
-    URL: `${defaultMessage} there is any Token URL`,
+    TOKEN_URL: `${defaultMessage} there is any Token URL`,
   };
   throw Error(messages[type]);
+};
+
+const getDummyAirlineData = async (token: string) => {
+  // dictionaries.carriers
+  // 1.
+  const originLocationCode = 'SEO';
+  const destinationLocationCode = 'TOK';
+
+  try {
+    const url = `${process.env.AMADEUS_BASE_URL}/shopping/flight-offers?originLocationCode=${originLocationCode}&destinationLocationCode=${destinationLocationCode}&departureDate=2024-03-20&adults=1&nonStop=true&max=250`;
+    const data = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return data.json();
+  } catch (err) {
+    console.log('err', err);
+  }
 };
