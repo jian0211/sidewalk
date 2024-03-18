b2865af5c3dafbfb73ad80a6146f06f3e86e0065
jian0211 <myunggeun222@gmail.com>
2024-03-18 21:35:40 +0900

    [data] airlineの dummy data生成

---
+++ prisma/dummy/airlines.ts
@@ -0,0 +1,19 @@
+/**
+ * checkList
+ *  INC -> NRT o
+ *  INC -> HND
+ *  INC -> KIX
+ *  INC -> FUK
+ *  INC -> NGO
+ *  INC -> ....
+ */
+
+const carriersDummy = {
+  JL: 'JAPAN AIRLINES',
+  TW: 'TWAY AIR',
+  H1: 'HAHN AIR SYSTEMS',
+  KE: 'KOREAN AIR',
+  NH: 'ALL NIPPON AIRWAYS',
+  OZ: 'ASIANA AIRLINES',
+  '7C': 'JEJU AIR',
+};
