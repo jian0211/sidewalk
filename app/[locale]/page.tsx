4622c8430602b789a249698aceb04fb9734b91fc
jian0211 <myunggeun222@gmail.com>
2024-04-11 21:16:03 +0900

    [main-page]: メインページ空作成

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home-page | Sidewalk',
+};
+
+const HomePage = async () => {
+  return <div> HomePAge</div>;
+};
+export default HomePage;
