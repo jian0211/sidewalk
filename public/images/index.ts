691915a27e6493e34e3745e4e0f7f2cc68ee09db
jian0211 <myunggeun222@gmail.com>
2024-04-30 18:28:23 +0900

    [dashboard]: image 追加

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,5 +1,13 @@
 import { FileProps, importAll } from '@/utils/import';
 
-export default importAll(require.context('./airlines', false, /\.png$/)).map(
-  (r: any) => r.default,
-) as FileProps[];
+const allImages = () => {
+  const airlineImages = importAll(
+    require.context('./airlines', false, /\.png$/),
+  ).map((r: any) => r.default) as FileProps[];
+  const sceneryImages = importAll(
+    require.context('./scenery', false, /\.jpg$/),
+  ).map((r: any) => r.default) as FileProps[];
+  return [...airlineImages, ...sceneryImages];
+};
+
+export default allImages();
