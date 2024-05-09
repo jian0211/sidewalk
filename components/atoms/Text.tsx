12d99b8f0005354f37b9bd4017d1ef51f2a95abb
jian0211 <myunggeun222@gmail.com>
2024-05-09 23:52:12 +0900

    props type 修正

--- components/atoms/Text.tsx
+++ components/atoms/Text.tsx
@@ -1,30 +1,23 @@
 import * as stylex from '@stylexjs/stylex';
 import { StyleXArray } from '@stylexjs/stylex/lib/StyleXTypes';
 import { DesignProps, designStyles } from '../styles';
-import { Prettier } from '@/types/common';
 import { fontSizing, fontWeight } from '../../styles/globalTokens.stylex';
-
-type TextCssProps = Prettier<Pick<DesignProps, 'color' | 'font'>>;
 
 type TextProps = {
   xstyle?: StyleXArray<any>;
-} & TextCssProps &
-  React.ComponentProps<'p'>;
+  colorProps?: DesignProps['color'];
+  fontProps?: DesignProps['font'];
+} & React.ComponentProps<'p'>;
 
 export const Text = (props: TextProps) => {
-  const { color, font, xstyle, ...rest } = props;
+  const { colorProps, fontProps, xstyle, ...rest } = props;
   return (
     <p
       {...rest}
       {...stylex.props(
         styles.text,
-        color && designStyles['color'](color),
-        font &&
-          designStyles['font']({
-            fontSize: font.fontSize,
-            fontWeight: font.fontWeight,
-            textDecoration: font.textDecoration,
-          }),
+        colorProps && designStyles['color'](colorProps),
+        fontProps && designStyles['font'](fontProps),
         xstyle,
       )}
     />
