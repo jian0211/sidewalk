bc97177711a2378ecf466cbd5b8241e1b906dcd7
jian0211 <myunggeun222@gmail.com>
2024-05-14 00:59:47 +0900

    FlightにWrapperコンポネント作成

--- components/templates/flights/noFlightData/NoFlightData.tsx
+++ components/templates/flights/noFlightData/NoFlightData.tsx
@@ -8,30 +8,33 @@ import {
   fontWeight,
   palette,
 } from '../../../../styles/globalTokens.stylex';
+import { Flights } from '../components';
 
 type NoFlightDataProps = React.ComponentProps<'div'>;
 
 export const NoFlightData = (props: NoFlightDataProps) => {
   const t = useTranslatedWord('flights.nodata');
   return (
-    <div {...props} {...stylex.props(styles['noData'])}>
-      <Image
-        src={worriedPlaneImage}
-        alt="worriedPlane"
-        width={400}
-        style={{ borderRadius: '50%' }}
-      />
-      <B
-        fontProps={{
-          fontSize: 'large',
-          fontWeight: 'bold',
-          textDecoration: 'none',
-        }}
-      >
-        {t('message')}
-      </B>
-      <p {...stylex.props(styles['helpMessage'])}>{t('helpMessage')}</p>
-    </div>
+    <Flights.Wrapper>
+      <div {...props} {...stylex.props(styles['noData'])}>
+        <Image
+          src={worriedPlaneImage}
+          alt="worriedPlane"
+          width={400}
+          style={{ borderRadius: '50%' }}
+        />
+        <B
+          fontProps={{
+            fontSize: 'large',
+            fontWeight: 'bold',
+            textDecoration: 'none',
+          }}
+        >
+          {t('message')}
+        </B>
+        <p {...stylex.props(styles['helpMessage'])}>{t('helpMessage')}</p>
+      </div>
+    </Flights.Wrapper>
   );
 };
 
