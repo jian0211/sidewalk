a9fa2fcb7388b89b58d6eccf53916176a3dc113a
jian0211 <myunggeun222@gmail.com>
2024-05-19 12:57:20 +0900

    チケットがない時のページ作成

---
+++ components/templates/flights/flightTicket/NoAnyFlightTicket.tsx
@@ -0,0 +1,26 @@
+import { Flex } from '@/components/atoms/Flex';
+import { Text } from '@/components/atoms/Text';
+import { useTranslatedWord } from '@/hooks/useTranslatedWord';
+
+type NoAnyFlightTicketProps = React.ComponentProps<'div'>;
+
+export const NoAnyFlightTicket = (props: NoAnyFlightTicketProps) => {
+  const t = useTranslatedWord('flights.notFound');
+  return (
+    <Flex
+      {...props}
+      flexProps={{
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+      sizeProps={{ height: '100%' }}
+    >
+      <Text
+        colorProps={{ color: 'brightOrange' }}
+        fontProps={{ fontSize: 'medium', fontWeight: 'bold' }}
+      >
+        {t('ticket')}
+      </Text>
+    </Flex>
+  );
+};
