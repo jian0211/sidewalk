9475ca376c8a2ac2fa56b8544867a0092b776b9b
jian0211 <myunggeun222@gmail.com>
2024-05-20 12:04:16 +0900

    airlines api 修正

--- app/api/airlines/route.ts
+++ app/api/airlines/route.ts
@@ -1,15 +1,16 @@
 import { db } from '@/prisma/prisma';
 import { Prisma } from '@prisma/client';
 import { NextApiRequest } from 'next';
+import { NextResponse } from 'next/server';
 
 export async function GET(req: Request & NextApiRequest) {
   console.log('Get airline Data');
   try {
     const airlines: Prisma.AirlineCreateInput[] = await db.airline.findMany();
-    return Response.json({ responseData: airlines });
+    return NextResponse.json({ responseData: airlines });
   } catch (err) {
     console.log('err', err);
-    Response.json(err);
+    NextResponse.json(err);
   }
   console.log('Done airline Data');
 }
